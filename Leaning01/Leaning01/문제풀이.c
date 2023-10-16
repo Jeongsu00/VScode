08ad64bdf46936dbcc0695fe44a97ff90bdcdbe9
jungsu00 <hakimz@naver.com>
2023-10-16 19:03:20 +0900

    23.10.16

---
+++ Leaning01/Leaning01/문제풀이.c
@@ -0,0 +1,61 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//	
+//	int s;
+//
+//	s = 52;
+//	printf("1년은 %d 주에 해당된다.",s);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void) {
+//
+//	printf("Baa Baa Black Sheep.");
+//	printf("Have you any wool\n");
+//	printf("Begone!\n0 creature of Lard!");
+//	printf("What?\nNo/nfish?\n");
+//
+//	int num;
+//	num = 2;
+//
+//	printf("%d + %d = %d", num, num, num + num);
+//}
+
+//#include <stdio.h>
+//
+//int main(void) {
+//
+//	int num1 = 3020;
+//	int num2 = 350;
+//
+//	printf("There were %d words and %d lines.", num1, num2);
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//	int a, b;
+//
+//	a = 5;
+//	b = 2;		// a = 5, b = 2
+//	b = a;		// a = 5, b = 5
+//	a = b;		// a = 5, b = 5
+//
+//	printf("a = %d, b = %d", a, b);
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//	int x, y;
+//
+//	x = 10;
+//	y = 5;			// x = 10, y = 5 
+//	y = x + y;		// x = 10, y = 15
+//	x = x * y;		// x = 150, y = 15
+//
+//	printf("x = %d, y = %d", x, y);
+//}
---
+++ Leaning01/Leaning01/함수 추가 선언.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//
+//void butler(void);			//함수를 선언
+//
+//int main(void) {
+//	printf("butler 함수 밖에 있느냐?\n");
+//	butler();
+//	printf("그래, 차 한 잔 내오고, DVD도 가져오너라.\n");
+//}
+//
+//void butler(void) {
+//	printf("부르셨습니까? 주임님!\n");
+//}
