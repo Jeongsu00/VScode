dfa19f105cc25d1d0609993094fac12758601e0e
jungsu00 <hakimz@naver.com>
2023-10-19 16:49:50 +0900

    23.10.19

---
+++ Leaning01/Leaning01/Programming exercises2.c
@@ -0,0 +1,62 @@
+//#include <stdio.h>
+//
+//void main() {
+//	printf("Gustav Mahler\n");
+//	printf("Gustav\nMahler\n");
+//	printf("Gustav ");
+//	printf("Mahler");
+//}
+
+//#include <stdio.h>
+//
+//void jolly();
+//void deny();
+//
+//void main() {
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//}
+//
+//void jolly() {
+//	printf("For he's a jolly good fellow!\n");
+//}
+//
+//void deny() {
+//	printf("Which nobody can deny!\n");
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//	int x = 0;
+//	for (x = 0; x < 3; x++) {
+//		for (int y = 0; y < 3 - x; y++){
+//			printf("smile! ");
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//
+//int one_three();
+//int two();
+//
+//int main() {
+//
+//	printf("starting now : \n");
+//	one_three();
+//	printf("done!");
+//}
+//
+//int one_three() {
+//	printf("one\n");
+//	two();
+//	printf("three\n");
+//}
+//
+//int two() {
+//	printf("two\n");
+//}
