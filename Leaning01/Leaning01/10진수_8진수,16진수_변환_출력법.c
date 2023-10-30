bd38a1a93211b95d7d8f24a63038051d20565c3f
jungsu00 <hakimz@naver.com>
2023-10-30 18:52:58 +0900

    23.10.30

---
+++ Leaning01/Leaning01/10진수_8진수,16진수_변환_출력법.c
@@ -0,0 +1,8 @@
+//#include <stdio.h>
+//
+//int main() {
+//	int x = 100;
+//
+//	printf("10진수 = %d,  8진수 = %o,  16진수 = %x \n", x, x, x);
+//	printf("10진수 = %d,  8진수 = %#o,  16진수 = %#x \n", x, x, x);
+//}
---
+++ Leaning01/Leaning01/char.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+//
+//int main() {
+//	char broiled1, broiled2, broiled3;
+//
+//	broiled1 = 'T';
+//	printf("%c \n", broiled1);	// OK
+////	broiled2 = T;
+////	printf("%c \n", broiled2);	// NO	T를 변수라고 생각한다.
+//	broiled3 = "T";
+//	printf("%c \n\n", broiled3);	// NO	T를 문자열이라고 생각한다.
+//
+//	char grade = 65;
+//	printf("%c \n", grade);		// ASTII코드를 바탕으로 'A'를 출력
+//
+//	char grade1 = 'B';
+//	printf("%d \n", grade1);	// ASTII코드를 바탕으로 66을 출력
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <Stdio.h>
+//
+//int main() {
+//	char ch;
+//
+//	printf("키보드를 영문 모드로 놓고, 문자 하나를 입력하시오. \n");
+//	scanf("%c", &ch);
+//	printf("문자 %c에 대응하는 코드값은 %d이다. \n", ch, ch);
+//}
